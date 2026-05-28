c719471de9fcbd33cd40327df3bfcabb241caa6d
divyam-vns <divyam.vns@gmail.com>
2026-05-28 18:34:32 +0000

    Step 7 complete: spatial neighborhood analysis + full checkpoint added

--- scripts/spatial_analysis.py
+++ scripts/spatial_analysis.py
@@ -1,8 +1,11 @@
 
 import squidpy as sq
+import scanpy as sc
 
 def spatial_analysis(adata):
+
     sq.gr.spatial_neighbors(adata)
     sq.gr.nhood_enrichment(adata, cluster_key='leiden')
     sq.gr.spatial_autocorr(adata, mode='moran')
+
     return adata
